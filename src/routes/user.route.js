12258c13d7787e2945041cab098cbef99ea1def9
tthienduc13 <ducnltdev@gmail.com>
2023-11-02 02:13:29 +0700

    update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,12 +3,14 @@ import userController from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/profile", userController.getAllUsers);
-router.get("/mention-data", userController.getMentionData);
 router.get("/profile/:user_id", userController.getUserInfo);
 router.delete("/profile/:user_id", userController.deleteUser);
 router.post("/profile/update-avatar/:user_id", userController.updateAvatar);
 router.post("/profile/update-info/:user_id", userController.updateInfo);
+
+router.get("/members", userController.getAllUsers);
+router.get("/students", userController.getAllStudents);
+router.get("/mentors", userController.getAllMentors);
 
 router.get("/profile-info/:user_id", userController.getUserProfile);
 router.patch("/profile/update-bio/:user_id", userController.updateBio);
