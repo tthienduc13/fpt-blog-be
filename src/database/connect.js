016e1eec1f8bfd60220afc307e65dd6186bb6e00
tthienduc13 <ducnltdev@gmail.com>
2023-11-15 14:28:03 +0700

    fix

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,16 +1,16 @@
 import mysql2 from "mysql2";
 import { config as configDotenv } from "dotenv";
 configDotenv();
-const DB_PORT = 3306;
-const DB_HOST = "localhost";
-const DB_USER = "root";
-const DB_PASS = "123456";
-const MYSQL_DB = "fptblog";
-// const DB_PORT = 43974;
-// const DB_HOST = "monorail.proxy.rlwy.net";
+// const DB_PORT = 3306;
+// const DB_HOST = "localhost";
 // const DB_USER = "root";
-// const DB_PASS = "gh15F6gbCHH6EaDHDc46EG2bCAH31AAa";
+// const DB_PASS = "123456";
 // const MYSQL_DB = "fptblog";
+const DB_PORT = 43974;
+const DB_HOST = "monorail.proxy.rlwy.net";
+const DB_USER = "root";
+const DB_PASS = "gh15F6gbCHH6EaDHDc46EG2bCAH31AAa";
+const MYSQL_DB = "fptblog";
 export const db = mysql2.createConnection({
   host: DB_HOST,
   port: DB_PORT,
