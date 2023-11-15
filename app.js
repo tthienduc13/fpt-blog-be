f26fe4bf782593ae65dc390fb197b74ee566a072
tthienduc13 <ducnltdev@gmail.com>
2023-11-15 12:18:26 +0700

    Deployed

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ initRoutes(app);
 const port = process.env.PORT || 3000;
 
 // Listen on `port` and 0.0.0.0
-app.listen(port, "0.0.0.0", function () {
+server.listen(port, "0.0.0.0", function () {
   console.log(`App is listening on ${port}`);
 });
